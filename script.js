0971bf3ab9aa60bcd3b4cc7cbdf03b5d62e576eb
Pedro Augusto <peaugusto.coelho@gmail.com>
2021-07-30 10:26:57 -0300

    Frist Commit

---
+++ script.js
@@ -0,0 +1,95 @@
+//Initial Data
+let areas = {
+  a: null,
+  b: null,
+  c: null,
+}
+
+
+//Events
+document.querySelectorAll('.item').forEach(item => {
+  item.addEventListener('dragstart' , dragStart);
+  item.addEventListener('dragend' ,   dragEnd);
+})
+
+document.querySelectorAll('.area').forEach(area => {
+  area.addEventListener('dragover' , dragOver);
+  area.addEventListener('dragleave' , dragLeave);
+  area.addEventListener('drop' , drop);
+})
+
+document.querySelector('.neutralArea').addEventListener('dragover' , dragOverNeutral);
+document.querySelector('.neutralArea').addEventListener('dragleave' , dragleaveNeutral);
+document.querySelector('.neutralArea').addEventListener('drop' , dropNeutral);
+
+
+// Functions Item
+function dragStart(e){
+  console.log('ola');
+  e.currentTarget.classList.add('dragging');
+}
+
+function dragEnd(e){
+  e.currentTarget.classList.remove('dragging');
+}
+
+// Functions Area
+function dragOver(e){
+  e.preventDefault();
+  if(e.currentTarget.querySelector('.item') === null){
+  e.currentTarget.classList.add('hover');
+  };
+}
+
+function dragLeave(e){
+  e.currentTarget.classList.remove('hover');
+}
+
+function drop(e){
+  e.currentTarget.classList.remove('hover');
+  if(e.currentTarget.querySelector('.item') === null){
+    let dragItem = document.querySelector('.item.dragging');
+    e.currentTarget.appendChild(dragItem);
+    updatesAreas();
+  }
+}
+
+//Functions NeutralArea
+function dragOverNeutral(e){
+  e.preventDefault();
+  e.currentTarget.classList.add('hover');
+}
+
+function dragleaveNeutral(e){
+  e.currentTarget.classList.remove('hover');
+}
+
+function dropNeutral(e){
+  e.currentTarget.classList.remove('hover');
+  let dragItem = document.querySelector('.item.dragging');
+  e.currentTarget.appendChild(dragItem);
+  updatesAreas();
+}
+
+
+//Logic Functions
+
+function updatesAreas(){
+  document.querySelectorAll('.area').forEach(area => {
+    let name = area.getAttribute('data-name');
+
+    if(area.querySelector('.item') !== null ){
+      areas[name] = area.querySelector('.item').innerHTML;
+    }else{
+      areas[name] = null;
+    }
+  });
+  if(areas.a === '1' && areas.b === '2' && areas.c === '3'){
+    document.querySelector('.areas').classList.add('correct');
+  }else if(areas.a&&areas.b&&areas.c){
+    document.querySelector('.areas').classList.add('error');
+  }else{
+    document.querySelector('.areas').classList.remove('correct');
+    document.querySelector('.areas').classList.remove('error');
+  }
+}
